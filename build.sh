c8c0b2b43fb65185779a1e4788e0fab174cc5561
Matthew Holt <mholt@users.noreply.github.com>
2026-03-10 10:08:42 -0600

    Fix build script

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-if [ -z "$CADDY_PLUGINS" ]; then
-	cp /usr/bin/caddy /usr/local/bin/caddy-custom
-	exit 0
-fi
-
 xcaddy build \
-	$(echo "$CADDY_PLUGINS" | tr ' \n' '\n' | sed '/^$/d' | sed 's/^/--with /') \
-	--output /usr/local/bin/caddy-custom
+    $(echo "$CADDY_PLUGINS" | tr ' \n' '\n' | sed '/^$/d' | sed 's/^/--with /') \
+    --output /usr/local/bin/caddy-custom
